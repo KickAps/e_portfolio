3845e5103f98eb4382771154ec31d413db663f3f
KickAps <flo.berson06@gmail.com>
2020-10-20 08:47:27 +0200

    navbar changes / user form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,11 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -17,14 +18,21 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['autofocus' => true]
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
-                'mapped' => false,
                 'label' => 'Mot de passe',
+                'mapped' => false,
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -37,8 +45,13 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('work')
-            ->add('description')
+            ->add('work', TextType::class, [
+                'label' => 'Travail'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => ['rows' => 5, 'cols' => 100]
+            ])
         ;
     }
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +14,20 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
-            ->add('password')
-            ->add('work')
-            ->add('description')
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['autofocus' => true]
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('work', TextType::class, [
+                'label' => 'Travail'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => ['rows' => 5, 'cols' => 100]
+            ])
         ;
     }
 
