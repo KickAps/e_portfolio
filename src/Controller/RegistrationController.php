4996fee5b9362571986d77932700df1f480a08a0
KickAps <flo.berson06@gmail.com>
2020-10-26 18:17:35 +0100

    fix mailer dns

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,15 +48,18 @@ class RegistrationController extends AbstractController
             $em->persist($user);
             $em->flush();
 
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('noreplay@eportfolio.com', 'Eportfolio'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-            // do anything else you need here, like send an email
+            if ($_ENV['MAILER_DSN'] !== "" and $_ENV['MAILER_DSN'] !== "smtp://localhost")
+            {
+                // generate a signed url and email it to the user
+                $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                    (new TemplatedEmail())
+                        ->from(new Address('noreplay@eportfolio.com', 'Eportfolio'))
+                        ->to($user->getEmail())
+                        ->subject('Please Confirm your Email')
+                        ->htmlTemplate('registration/confirmation_email.html.twig')
+                );
+                // do anything else you need here, like send an email
+            }
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
