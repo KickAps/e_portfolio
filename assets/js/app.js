742ee972241a970c25da99fd46e29e8e9bd42063
KickAps <flo.berson06@gmail.com>
2020-11-09 12:45:44 +0100

    favicon

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,8 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import '../scss/app.scss';
 
+import '../favicon/favicon.ico';
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
 import 'bootstrap';
