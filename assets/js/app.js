462cc6b3e8001a3a2050039693f06858304b6e62
KickAps <flo.berson06@gmail.com>
2020-11-22 16:59:57 +0100

    fix browser back button after logout

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,27 @@ import '../favicon/favicon.ico';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
 import 'bootstrap';
+
+// Disable the browser back button feature when logout
+noReturnOnLogout();
+
+function noReturnOnLogout() {
+    if (window.location.href.match(/\/login$/i)) {
+        drownHistory();
+        window.location = window.location.href + "?logout";
+    }
+
+    if (window.location.href.match(/\/login\?logout$/i)) {
+        drownHistory();
+    }
+}
+
+function drownHistory() {
+    for (var i = 0; i < 15; i++) {
+        window.history.pushState(null, "", window.location.href);
+    }
+    window.onpopstate = function () {
+        document.location.reload(true);
+        window.history.pushState(null, "", window.location.href);
+    };
+}
