ba4470494ac7a81e1f02b3f47da94b89cf76a1b9
KickAps <flo.berson06@gmail.com>
2020-12-04 10:49:38 +0100

    fix email / htaccess

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -43,7 +43,7 @@ class RegistrationController extends AbstractController
         // generate a signed url and email it to the user
         $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
             (new TemplatedEmail())
-                ->from(new Address('noreply-e-porfolio-plus@gmail.com', 'E-Portfolio+'))
+                ->from(new Address('noreply@eportfolio-plus.fr', 'ePortfolio+'))
                 ->to($user->getEmail())
                 ->subject('Merci de confirmer votre adresse mail')
                 ->htmlTemplate('registration/confirmation_email.html.twig')
