ee814d2cb0846024b2a31eea19ed97770ce31bce
KickAps <flo.berson06@gmail.com>
2022-02-04 17:27:00 +0100

    Fix user link

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -6,11 +6,6 @@ $(document).ready(function()
     var copyLinkInput = $('input[name ="personnal-link-input"]');
     // If the current page is the profile page
     if (copyLinkInput.length !== 0) {
-
-        // Set input width based on characters count
-        var width = copyLinkInput[0].value.length;
-        copyLinkInput[0].style.width = width*9 + "px";
-
         // Change cursor
         copyLinkInput.mouseover(function(e){
             copyLinkInput[0].style.cursor = "pointer";
